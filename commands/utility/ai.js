62ee6b8983ce09bb7e48a7342de0d5f7e2f9041a
Dylan <dmichaelmccormick@gmail.com>
2025-01-31 18:23:10 -0600

    slightly different command for fast ai responses to any topics

---
+++ commands/utility/ai.js
@@ -0,0 +1,36 @@
+// /commands/ask.js
+const { SlashCommandBuilder } = require('discord.js');
+const {  OPENAI_API_KEY } = require('../../config.json');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+
+const axios = require('axios');
+
+async function getOpenAIResponse(prompt) {
+    try {
+        const genAI = new GoogleGenerativeAI(OPENAI_API_KEY);
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const result = await model.generateContent(`'With less than two thousand characters answer the following query: ' + ${prompt}`);
+
+        return result.response.text();
+    } catch (error) {
+        console.error('Error with OpenAI API:', error);
+        return 'Sorry, something went wrong with the AI request.';
+    }
+}
+
+module.exports = {
+   data: new SlashCommandBuilder()
+    .setName('irobot')
+    .setDescription('Get an AI response')
+    .addStringOption(option =>
+        option.setName('query')
+            .setDescription('Your question to the AI')
+            .setRequired(true)
+    ),
+    async execute(interaction) {
+        const userQuery = interaction.options.getString('query');
+        const aiResponse = await getOpenAIResponse(userQuery);
+        await interaction.reply(aiResponse);
+    },
+};
